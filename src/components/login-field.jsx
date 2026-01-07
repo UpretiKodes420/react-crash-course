f865edca1c5fd6c7cd8ea740f3489d5da4f4ac13
Dipen420 <lolode6561@gmail.com>
2026-01-07 11:31:43 +0000

    validation added in password section

--- src/components/login-field.jsx
+++ src/components/login-field.jsx
@@ -1,11 +1,20 @@
-export default function LoginField({ inputtype, fieldname }) {
-    return (
-        <div className="mt-7">
-            <div>
-                <label className="text-2xl text-white text-shadow-sm">{fieldname + ":"}</label>
-            </div>
+import { forwardRef } from "react";
+const LoginField = forwardRef(function LoginField({ inputtype, fieldname, ...tracer }, ref) {
+  return (
+    <div className="mt-7">
+      <div>
+        <label className="text-2xl text-white text-shadow-sm">
+          {fieldname + ":"}
+        </label>
+      </div>
 
-            <input type={inputtype} className="bg-white mt-1 w-70 border p-2 rounded-xl shadow-xl/30 " />
-        </div>
-    )
-}
+      <input
+        type={inputtype}
+        className="bg-white mt-1 w-70 border p-2 rounded-xl shadow-xl/30 "
+        ref={ref}
+        {...tracer}
+      />
+    </div>
+  );
+})
+export default LoginField 
--- src/components/login-section.jsx
+++ src/components/login-section.jsx
@@ -1,25 +1,35 @@
-import LoginField from "./login-field"
-import Mybutton from "./button"
-
-
-
-
+import LoginField from "./login-field";
+import Mybutton from "./button";
+import { useForm } from "react-hook-form";
 export default function LoginSection() {
-    return (
-        <div className=" justify-center  mt-4 gap-4 p-5">
-            <label className="text-4xl font-bold text-white">Login</label>
-            <LoginField inputtype={"text"} fieldname={"username"} />
-            <LoginField inputtype={"password"} fieldname={"password"} />
-            <div className=" flex justify-center">
-                <form >
-                    <Mybutton label={"login"} type={"submit"} />
-                    <Mybutton label={"signup"} type={"submit"} />
-                </form>
-
-            </div>
-
-        </div>
-
-
-    )
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  return (
+    <div className=" justify-center  mt-4 gap-4 p-5">
+      <label className="text-4xl font-bold text-white">Login</label>
+      <LoginField
+        inputtype={"text"}
+        fieldname={"username"}
+        {...register("userName")}
+      />
+      <LoginField
+        inputtype={"password"}
+        fieldname={"password"}
+        {...register("Password", { "required": true, "maxLength": 20 })}
+      />
+      <div className=" flex justify-center">
+        <form onSubmit={handleSubmit(validation)}>
+          <Mybutton label={"login"} type={"submit"} />
+          <Mybutton label={"signup"} type={"submit"} />
+        </form>
+      </div>
+    </div>
+  );
+}
+function validation() {
+  console.log("Data submitted sucessfully")
 }
